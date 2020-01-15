4bdd92bf05256a28b196e5f02656198793aa052b
Vijay1110 <33751735+Vijay1110@users.noreply.github.com>
2020-01-15 06:35:22 +0530

    Add files via upload

---
+++ ReactReduxNoThunk/src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { mount } from "enzyme";
+import CommentBox from "components/CommentBox";
+import Root from "Root";
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <CommentBox />
+    </Root>
+  );
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("has a text area and a button", () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("textarea").length).toEqual(1);
+});
+
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "new comment" }
+    });
+
+    wrapped.update();
+  });
+
+  it("has a text area that user can type in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  });
+
+  it("when form is submitted, text area gets emptied", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
+});
+
+// it("has a text area that user can type in", () => {
+//     wrapped.find("textarea").simulate("change", {
+//       target: { value: "new comment" }
+//     });
+
+//     wrapped.update();
+
+//     expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+//   });
+
+//   it("when form is submitted, text area gets emptied", () => {
+//     wrapped.find("textarea").simulate("change", {
+//       target: { value: "new comment" }
+//     });
+
+//     wrapped.update();
+//     expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+//     wrapped.find("form").simulate("submit");
+//     wrapped.update();
+//     expect(wrapped.find("textarea").prop("value")).toEqual("");
+//   });
